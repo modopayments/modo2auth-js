a8767fe0b0334dd968bd55fb388cf6efeeb07a7e
Adam Schnaare <adam.schnaare@modopayments.com>
2020-05-26 11:12:10 -0500

    Refactor utils as private methods on class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const { makeHeader, makePayload, makeSignature } = require('./utils.js')
+const crypto = require('crypto') // https://nodejs.org/api/crypto.html
+const base64url = require('base64url') // https://www.npmjs.com/package/base64url
+// const { makePayload, makeSignature } = require('./utils.js')
 
 module.exports = class Modo2Auth {
   constructor({ api_identifier, api_secret }) {
@@ -6,12 +8,53 @@ module.exports = class Modo2Auth {
     this.api_secret = api_secret
   }
 
+  _makeHeader() {
+    const data = {
+      alg: 'HS256',
+      typ: 'JWT',
+    }
+    return base64url(JSON.stringify(data))
+  }
+
+  _makePayload(api_uri, api_identifier, body, iat) {
+    // iat is exposed to allow to proper testing of the function
+    const timestamp = Math.round(new Date().getTime() / 1000) // in seconds
+    const body_hash = this._encodeHash(body) // hex digest of sha256 of data
+
+    const payload = {
+      iat: iat || timestamp,
+      api_identifier,
+      api_uri,
+      body_hash,
+    }
+
+    return base64url(JSON.stringify(payload))
+  }
+
+  _encodeHash(data) {
+    return crypto.createHash('sha256').update(data).digest('hex')
+  }
+
+  _makeSignature(header, payload, secret) {
+    const data = `${header}.${payload}`
+    const digest = crypto.createHmac('sha256', secret).update(data).digest('base64')
+
+    return this._sanitizeString(digest)
+  }
+
+  _sanitizeString(url) {
+    url = url.replace(/[+]/g, '-')
+    url = url.replace(/[/]/g, '_')
+    url = url.replace(/[=]/g, '')
+    return url
+  }
+
   getToken(api_uri, body, iat) {
     body = body || '' // ensure body is defined
     body = typeof body == 'object' ? JSON.stringify(body) : body // verify body is stringified JSON
-    const header = makeHeader()
-    const payload = makePayload(api_uri, this.api_identifier, body, iat)
-    const signature = makeSignature(header, payload, this.api_secret)
+    const header = this._makeHeader()
+    const payload = this._makePayload(api_uri, this.api_identifier, body, iat)
+    const signature = this._makeSignature(header, payload, this.api_secret)
     return `MODO2 ${header}.${payload}.${signature}`
   }
 
--- tests/unit/index.test.js
+++ tests/unit/index.test.js
@@ -7,46 +7,96 @@ describe('Modo2Auth', () => {
   const iat = 1590072685
   const body = { someKey: 'someValue' }
 
-  describe('constructor', () => {
-    it('should create an instance of Modo2Auth', () => {
-      expect(modo2Auth.constructor.name).toBe('Modo2Auth')
+  describe('API', () => {
+    describe('constructor', () => {
+      it('should create an instance of Modo2Auth', () => {
+        expect(modo2Auth.constructor.name).toBe('Modo2Auth')
+      })
+    })
+
+    describe('getToken', () => {
+      const expected =
+        'MODO2 eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTAwNzI2ODUsImFwaV9pZGVudGlmaWVyIjoiN2cwVUFwaXBNcHVKMVZPT09IV0pOSVpIN1ZaSU5iMDgiLCJhcGlfdXJpIjoiL3Rlc3QiLCJib2R5X2hhc2giOiI0NmI4ZGFkMWM2OWNiZDUwMGU5ZDFmZTJmMmVjZTM1N2M4NGM2ZTM2Y2U3YTg2MGJmMTQ2NzJiNGI3NDBhZjE5In0.sWjjz_MpnSv8Z31gNpEm1cmDhN7MK7Z3ix61RbDRL7g'
+      it('should return a MODO2 auth token (body as object)', () => {
+        const token = modo2Auth.getToken(api_uri, body, iat)
+
+        expect(token).toBe(expected)
+      })
+      it('should return a MODO2 auth token, (body as string)', () => {
+        const token = modo2Auth.getToken(api_uri, JSON.stringify(body), iat)
+
+        expect(token).toBe(expected)
+      })
+    })
+
+    describe('signRequest', () => {
+      const expected =
+        'MODO2 eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTAwNzI2ODUsImFwaV9pZGVudGlmaWVyIjoiN2cwVUFwaXBNcHVKMVZPT09IV0pOSVpIN1ZaSU5iMDgiLCJhcGlfdXJpIjoiL3Rlc3QiLCJib2R5X2hhc2giOiI0NmI4ZGFkMWM2OWNiZDUwMGU5ZDFmZTJmMmVjZTM1N2M4NGM2ZTM2Y2U3YTg2MGJmMTQ2NzJiNGI3NDBhZjE5In0.sWjjz_MpnSv8Z31gNpEm1cmDhN7MK7Z3ix61RbDRL7g'
+      const body = { someKey: 'someValue' }
+      it('should add a MODO2 Authorization header to a request (body as object)', () => {
+        const request = new MockRequest()
+        request.body = body
+        const signedRequest = modo2Auth.signRequest(api_uri, request, iat)
+
+        expect(signedRequest.headers['Authorization']).not.toBe(null)
+        expect(signedRequest.headers['Authorization']).toBe(expected)
+      })
+      it('should add a MODO2 Authorization header to a request (body as string)', () => {
+        const request = new MockRequest()
+        request.body = JSON.stringify(body)
+        const signedRequest = modo2Auth.signRequest(api_uri, request, iat)
+
+        expect(signedRequest.headers['Authorization']).not.toBe(null)
+        expect(signedRequest.headers['Authorization']).toBe(expected)
+      })
     })
   })
 
-  describe('getToken', () => {
-    const expected =
-      'MODO2 eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTAwNzI2ODUsImFwaV9pZGVudGlmaWVyIjoiN2cwVUFwaXBNcHVKMVZPT09IV0pOSVpIN1ZaSU5iMDgiLCJhcGlfdXJpIjoiL3Rlc3QiLCJib2R5X2hhc2giOiI0NmI4ZGFkMWM2OWNiZDUwMGU5ZDFmZTJmMmVjZTM1N2M4NGM2ZTM2Y2U3YTg2MGJmMTQ2NzJiNGI3NDBhZjE5In0.sWjjz_MpnSv8Z31gNpEm1cmDhN7MK7Z3ix61RbDRL7g'
-    it('should return a MODO2 auth token (body as object)', () => {
-      const token = modo2Auth.getToken(api_uri, body, iat)
+  describe('PRIVATE', () => {
+    const { api_identifier, api_secret } = mockCreds
+    const header = modo2Auth._makeHeader()
+    const api_uri = '/payouts/minimum?action=complete'
+    const iat = 1590072685
+    const bodyString = JSON.stringify(body)
+    const encoded = modo2Auth._encodeHash(bodyString)
+    const payload = modo2Auth._makePayload(api_uri, api_identifier, bodyString, iat)
+    const signature = modo2Auth._makeSignature(header, payload, api_secret)
 
-      expect(token).toBe(expected)
+    describe('_makeHeader', () => {
+      it('should return an encoded header', () => {
+        expect(header).toBe('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9')
+      })
     })
-    it('should return a MODO2 auth token, (body as string)', () => {
-      const token = modo2Auth.getToken(api_uri, JSON.stringify(body), iat)
 
-      expect(token).toBe(expected)
+    describe('_makePayload', () => {
+      it('should return an encoded payload', () => {
+        expect(payload).toBe(
+          'eyJpYXQiOjE1OTAwNzI2ODUsImFwaV9pZGVudGlmaWVyIjoiN2cwVUFwaXBNcHVKMVZPT09IV0pOSVpIN1ZaSU5iMDgiLCJhcGlfdXJpIjoiL3BheW91dHMvbWluaW11bT9hY3Rpb249Y29tcGxldGUiLCJib2R5X2hhc2giOiI0NmI4ZGFkMWM2OWNiZDUwMGU5ZDFmZTJmMmVjZTM1N2M4NGM2ZTM2Y2U3YTg2MGJmMTQ2NzJiNGI3NDBhZjE5In0'
+        )
+      })
     })
-  })
 
-  describe('signRequest', () => {
-    const expected =
-      'MODO2 eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTAwNzI2ODUsImFwaV9pZGVudGlmaWVyIjoiN2cwVUFwaXBNcHVKMVZPT09IV0pOSVpIN1ZaSU5iMDgiLCJhcGlfdXJpIjoiL3Rlc3QiLCJib2R5X2hhc2giOiI0NmI4ZGFkMWM2OWNiZDUwMGU5ZDFmZTJmMmVjZTM1N2M4NGM2ZTM2Y2U3YTg2MGJmMTQ2NzJiNGI3NDBhZjE5In0.sWjjz_MpnSv8Z31gNpEm1cmDhN7MK7Z3ix61RbDRL7g'
-    const body = { someKey: 'someValue' }
-    it('should add a MODO2 Authorization header to a request (body as object)', () => {
-      const request = new MockRequest()
-      request.body = body
-      const signedRequest = modo2Auth.signRequest(api_uri, request, iat)
-
-      expect(signedRequest.headers['Authorization']).not.toBe(null)
-      expect(signedRequest.headers['Authorization']).toBe(expected)
+    describe('_makeSignature', () => {
+      it('should return an encoded signature', () => {
+        expect(signature).toBe('lE865md6iVe42QyAGMpcm4bJntMACcDISfCxMrKzOuo')
+      })
     })
-    it('should add a MODO2 Authorization header to a request (body as string)', () => {
-      const request = new MockRequest()
-      request.body = JSON.stringify(body)
-      const signedRequest = modo2Auth.signRequest(api_uri, request, iat)
 
-      expect(signedRequest.headers['Authorization']).not.toBe(null)
-      expect(signedRequest.headers['Authorization']).toBe(expected)
+    describe('_encodeHash', () => {
+      it('should return a hashed string', () => {
+        expect(encoded).toBe(
+          '46b8dad1c69cbd500e9d1fe2f2ece357c84c6e36ce7a860bf14672b4b740af19'
+        )
+      })
+    })
+
+    describe('_sanitizeString', () => {
+      it('should correctly transform the url', () => {
+        const url = 'Test+/=Test+/=Test'
+        const sanitized = modo2Auth._sanitizeString(url)
+
+        expect(sanitized).toBe('Test-_Test-_Test')
+      })
     })
   })
 })
